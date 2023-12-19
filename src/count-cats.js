eecd54b989cf75ef508003bc1ff18f3bbea1a852
Polina Deniskina <polinaoioi67@gmail.com>
2023-12-19 21:31:37 +0300

    feat: solve a count cat ears task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix ) {
+  const CATS_EARS_STRING = '^^';
+  let countCatsEars = 0;
+  for (const matrixStr of matrix) {
+    for (const matrixStrElement of matrixStr) {
+      if (matrixStrElement === CATS_EARS_STRING) {
+        countCatsEars++;
+      }
+    }
+  }
+  return countCatsEars;
 }
 
 module.exports = {
