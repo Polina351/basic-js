c79377263a1e93bb2c2d5b4be5cffe9d3ab659bc
Polina Deniskina <polinaoioi67@gmail.com>
2023-12-20 16:09:47 +0300

    feat: solve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam( members ) {
+  if (!Array.isArray(members)) return false;
+
+  let res = '';
+  members.forEach(elem => {
+    if (typeof elem === 'string') {
+      res += elem.replace(/ /g, '').slice(0, 1);
+    }
+  });
+
+  return res.toUpperCase().split('').sort((a, b) => {
+    if (a < b) {
+      return -1;
+    }
+    if (a > b) {
+      return 1;
+    }
+    return 0;
+  }).join('') || false;
 }
 
 module.exports = {
