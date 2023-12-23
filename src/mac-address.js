fba1042ef03efecd1534a509600110e47ef66736
Polina Deniskina <polinaoioi67@gmail.com>
2023-12-24 00:37:07 +0300

    feat: solve mac-addres task

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address( n ) {
+  const regexp = /\p{Hex_Digit}\p{Hex_Digit}/u;
+  const array = n.split('-');
+  let isHex = true;
+
+  for (const arrayElement of array) {
+    if (regexp.test(arrayElement) === false) {
+      isHex = false;
+    }
+  }
+  return Boolean(isHex && array.length === 6);
 }
 module.exports = {
   isMAC48Address
