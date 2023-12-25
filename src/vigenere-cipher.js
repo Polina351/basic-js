319155c4b03e2b05cb3533be0dcb0f4f7e120ec0
Polina Deniskina <polinaoioi67@gmail.com>
2023-12-25 18:38:23 +0300

    feat: solve vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,68 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isReverse = true ) {
+    this.reverse = isReverse;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(string, key) {
+    if (!string || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    string = string.toUpperCase();
+    key = key.toUpperCase();
+
+    const alphabetPower = 26;
+    const codeSymbolA = 'A'.charCodeAt(0);
+
+    const res = []; // str or arr
+
+    for (let i = 0, j = 0; i < string.length; i++) {
+
+      const currentSymb = string[i];
+
+      if (/^[A-Z]$/.test(currentSymb)) {
+        const indexSymb = string.charCodeAt(i) - codeSymbolA;
+        const shift = key.charCodeAt(j % key.length) -codeSymbolA;
+
+        res.push(String.fromCharCode(codeSymbolA + (indexSymb + shift) % alphabetPower));
+        j++;
+      } else {
+        res.push(currentSymb);
+      }
+    }
+    return this.reverse ? res.join('') : res.reverse().join('');
+  }
+  decrypt(string, key) {
+    if (!string || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    string = string.toUpperCase();
+    key = key.toUpperCase();
+
+    const alphabetPower = 26;
+    const codeSymbolA = 'A'.charCodeAt(0);
+
+    const res = []; // str or arr
+
+    for (let i = 0, j = 0; i < string.length; i++) {
+
+      const currentSymb = string[i];
+
+      if (/^[A-Z]$/.test(currentSymb)) {
+        const indexSymb = string.charCodeAt(i) - codeSymbolA;
+        const shift = key.charCodeAt(j % key.length) - codeSymbolA;
+        const charCode = (indexSymb - shift + alphabetPower) % alphabetPower + codeSymbolA;
+
+        res.push(String.fromCharCode(charCode));
+        j++;
+      } else {
+        res.push(currentSymb);
+      }
+    }
+    return this.reverse ? res.join('') : res.reverse().join('');
   }
 }
 
